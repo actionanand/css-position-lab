92b0d2edef5ad20b970a324776852b2e1690c5bd
Anand raja <meetanandns@gmail.com>
2024-01-07 17:47:53 +0000

    initial

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('.parent')!.innerHTML = `
+  <div class="child-1">Navigation Bar</div>
+  <div class="child-2">Background Image</div>
+  <div class="child-3">Features</div>
+`
