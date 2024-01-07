4ae41312acff6fd5c7dac6c485e6a12161200801
Anand raja <meetanandns@gmail.com>
2024-01-07 18:17:34 +0000

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import htmlContent from './position.html?raw';
 import './style.css'
 
-document.querySelector<HTMLDivElement>('.parent')!.innerHTML = `
-  <div class="child-1">Navigation Bar</div>
-  <div class="child-2">Background Image</div>
-  <div class="child-3">Features</div>
-`
+document.querySelector<HTMLDivElement>('.parent')!.innerHTML = htmlContent;
+
+// document.querySelector('.parent').innerHTML = `
+//   <div>
+//     raw html content
+//   </div>
+// `
